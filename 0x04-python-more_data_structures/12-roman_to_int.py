b17d269a92aff15e8c210c737b856270a3fa1cb6
FavourMaca <favouronyekwere830@gmail.com>
2022-12-07 08:16:27 -0800

    More Structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,31 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string: str):
-    if roman_string is None or type(roman_string) != str:
+
+def roman_to_int(roman_string):
+    if not roman_string:
         return 0
-    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    numbers = [data[x] for x in roman_string] + [0]
-    rep = 0
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
+        return 0
 
-    for i in range(len(numbers) - 1):
-        if numbers[i] >= numbers[i+1]:
-            rep += numbers[i]
+    sum_n = 0
+    roman_n = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    len_n = len(roman_string)
+    for x in range(len_n):
+        if roman_n.get(roman_string[x], 0) == 0:
+            return 0
+
+        if (x != (len_n - 1) and
+                roman_n[roman_string[x]] < roman_n[roman_string[x + 1]]):
+            sum_n += roman_n[roman_string[x]] * -1
         else:
-            rep -= numbers[i]
-
-    return rep 
+            sum_n += roman_n[roman_string[x]]
+    return sum_n
